8f598608e47aa99feef2f46eff67e2cde31e9f9a
jennikins813 <jennikins813@yahoo.com>
2014-04-08 17:52:35 -0400

    Refactoring routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Photogur::Application.routes.draw do
+  resources :pictures
   root :to => "pictures#index"
-  get 'pictures' => 'pictures#index'
-
-  post 'pictures' => "pictures#create"    #this is a new line of code
-  get 'pictures/new' => 'pictures#new'    #this is also a new line of code
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-  patch 'pictures/:id' => "pictures#update"
-
-  get 'pictures/:id' => 'pictures#show', as: "picture"      #as:"picture" is the picture_path. 
-
-  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
 
 end
 
