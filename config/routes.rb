da83126ac4abab5674d38f79683229a53f809469
jennikins813 <jennikins813@yahoo.com>
2014-04-08 13:57:40 -0400

    Getting http://localhost:3000/pictures to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Photogur::Application.routes.draw do
+  get 'pictures' => 'pictures#index'
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +56,3 @@ Photogur::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
