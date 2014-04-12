dd8b7e9b95ae9dec43f6d542f68ad29c182b80ff
jennikins813 <jennikins813@yahoo.com>
2014-04-12 11:14:52 -0400

    Display most recent pictures on the index page

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,12 +1,8 @@
 class Picture < ActiveRecord::Base 			
 	scope :newest_first, -> { order("created_at DESC") }
 	scope :most_recent_five, -> { newest_first.limit(5) }
-	#scope :created_before, ->(time) {where("created_at < ?", time) }
-		#this can also be written as below:
-
-	def self.created_before(time)
-		where("created_at < ?",time)
-	end
+	scope :created_before, ->(time) {where("created_at < ?", time) }
+	
 end 										
 
 
